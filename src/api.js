b4fa9cd5316c99b0de9ac88b21f7c412349a4347
korniplodiks <korniplodiks@users.noreply.github.com>
2025-10-24 19:08:45 +0300

    build: fix broken tests

--- src/api.js
+++ src/api.js
@@ -185,3 +185,30 @@ class API {
 
 // Update 34
 module.exports = API;
+
+
+// API module for BugTracker
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 35
+module.exports = API;
