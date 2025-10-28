54e87f71e5ec28deaa8086dcddbd5927c7ca0c05
korniplodiks <korniplodiks@users.noreply.github.com>
2025-10-28 20:18:01 +0200

    perf: fix async handling

--- src/api.js
+++ src/api.js
@@ -23,3 +23,30 @@ class API {
 
 // Update 6
 module.exports = API;
+
+
+// API module for BugTracker
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 11
+module.exports = API;
