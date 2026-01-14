0e5f45d56e6a8c26c023e02a7896179c32f55726
korniplodiks <korniplodiks@users.noreply.github.com>
2026-01-15 00:37:23 +0200

    fix: fix null pointer exception

--- src/api.js
+++ src/api.js
@@ -266,3 +266,30 @@ class API {
 
 // Update 46
 module.exports = API;
+
+
+// API module for BugTracker
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 47
+module.exports = API;
